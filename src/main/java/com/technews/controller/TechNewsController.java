5f0ef7b0fdd57c461824feae8e15719aebd41795
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-05-21 13:58:32 -0400

    Added /comments @PostMapping method ( post comments)

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -1,5 +1,6 @@
 package com.technews.controller;
 
+import com.technews.model.Comment;
 import com.technews.model.Post;
 import com.technews.model.User;
 import com.technews.repository.CommentRepository;
@@ -125,6 +126,22 @@ public class TechNewsController {
             postRepository.save(tempPost);
 
             return "redirect:/dashboard";
+        }
+    }
+    @PostMapping("/comments")
+    public String createCommentCommentsPage(@ModelAttribute Comment comment, Model model, HttpServletRequest request) {
+
+        if (comment.getCommentText().isEmpty() || comment.getCommentText().equals(null)) {
+            return "redirect:/singlePostEmptyComment/" + comment.getPostId();
+        } else {
+            if (request.getSession(false) != null) {
+                User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                comment.setUserId(sessionUser.getId());
+                commentRepository.save(comment);
+                return "redirect:/post/" + comment.getPostId();
+            } else {
+                return "login";
+            }
         }
     }
 }

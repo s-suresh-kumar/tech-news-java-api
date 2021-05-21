acdb90f2b935bc6814a8fced9b9cc5b7bbb60734
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-05-21 13:44:36 -0400

    Added /users/login @PostMapping method

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -1,11 +1,18 @@
 package com.technews.controller;
 
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TechNewsController {
@@ -21,5 +28,36 @@ public class TechNewsController {
 
     @Autowired
     CommentRepository commentRepository;
+    @PostMapping("/users/login")
+    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
 
+        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "Email address and password must be populated in order to login!");
+            return "login";
+        }
+
+        User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
+        try {
+            if (sessionUser.equals(null)) {
+
+            }
+        } catch (NullPointerException e) {
+            model.addAttribute("notice", "Email address is not recognized!");
+            return "login";
+        }
+
+        // Validate Password
+        String sessionUserPassword = sessionUser.getPassword();
+        boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
+        if(isPasswordValid == false) {
+            model.addAttribute("notice", "Password is not valid!");
+            return "login";
+        }
+
+        sessionUser.setLoggedIn(true);
+        request.getSession().setAttribute("SESSION_USER", sessionUser);
+
+        return "redirect:/dashboard";
+    }
 }
